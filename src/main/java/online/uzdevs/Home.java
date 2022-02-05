aea51728b335030d4827b84500878202150d1370
jimhome_mac <jsr1611@outlook.com>
2022-02-05 22:50:18 +0900

    Initial commit

---
+++ src/main/java/online/uzdevs/Home.java
@@ -0,0 +1,15 @@
+package online.uzdevs;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Home extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.write("Welcome to our homepage!");
+    }
+}
